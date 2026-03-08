0dc1e30885f1e5a34ebcdecfff812f0fc9b80604
sashain6-netizen <sashain6@gmail.com>
2026-03-08 14:13:11 -0700

    update

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,11 +1,13 @@
 // This file overwrites the stock UV config.js
-
 self.__uv$config = {
   prefix: "/uv/service/",
+  // This is the missing piece!
+  bare: "https://tomp.app/", 
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
-  client: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.client.js",  bundle: "/uv/uv.bundle.js",
+  client: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",
   sw: "/uv/uv.sw.js",
 };
