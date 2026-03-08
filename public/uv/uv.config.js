60a112647aae103b4a055bdfe21ca5e6d01de37c
sashain6-netizen <sashain6@gmail.com>
2026-03-08 14:07:01 -0700

    update

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -5,8 +5,7 @@ self.__uv$config = {
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
+  client: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.client.js",  bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",
   sw: "/uv/uv.sw.js",
 };
